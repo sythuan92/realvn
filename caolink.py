d91a2b7f91b9903a3e87bbb2fd21ed1fe17ea688
sythuan92 <38721519+sythuan92@users.noreply.github.com>
2021-08-29 16:40:56 +0700

    update lan 2

--- caolink.py
+++ caolink.py
@@ -12,12 +12,43 @@ from openpyxl import load_workbook
 import openpyxl
 import time
 import random
-
 print('- Finish importing packages')
-proxy = "27.79.137.213:45360"
+#Gọi đăng nhập
+def loginbds():
+    # Task 1.1: Open Chrome and Access Linkedin login site
+    sleep(1)
+    urldn = 'https://batdongsan.com.vn/'
+    driver.get(urldn)
+    print('- hàm gọi dang nhap')
+    sleep(1)
+    # Task 1.2: Import username and password
+    credential = open('credentials.txt')
+    line = credential.readlines()
+    username = line[0]
+    password = line[1]
+    sleep(1)
+
+    # Task 1.2: Key in login credentials
+    dangnhap = driver.find_element_by_xpath("/html/body/div[1]/header/div[2]/div[1]/div[2]/a[1]")
+    dangnhap.click()
+    sleep(2)
+    email_field = driver.find_element_by_id('UserName')
+    email_field.send_keys(username)
+    print('- Finish keying in email')
+    sleep(1)
+
+    password_field = driver.find_element_by_id('Password')
+    password_field.send_keys(password)
+    print('- Finish keying in password')
+    sleep(2)
+
+    # Task 1.2: Click the Login button
+    signin_field = driver.find_element_by_id('btnLogin')
+    signin_field.click()
+    sleep(3)
+    print('- Finish Task 1: Login to bds')
 # setup selenium webdriver
 opt = webdriver.ChromeOptions()
-# opt.add_extension("Block-image_v1.1.crx")
 prefs = {"profile.managed_default_content_settings.images": 2}
 opt.add_experimental_option("prefs", prefs)
 opt.add_argument('--disable-gpu')
@@ -25,14 +56,16 @@ opt.add_argument("--window-size=1920,1080")
 opt.add_argument("--start-maximized")
 opt.add_argument('--disable-dev-shm-usage')
 opt.add_argument('--no-sandbox')
+opt.add_argument('--disable-notifications')
 opt.add_argument('--ignore-certificate-errors')
+opt.add_argument("--disable-browser-side-navigation")
 # opt.add_argument('--headless')
-opt.add_argument('--proxy-server={}'.format(proxy))
 opt.add_argument("user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.90 Safari/537.36")
-# driver = webdriver.Chrome(options=opt)
 driver = webdriver.Chrome(options=opt)
+#login bds
+loginbds()
 sleep(2)
-url = 'https://batdongsan.com.vn/nha-dat-cho-thue-da-nang'
+url = 'https://batdongsan.com.vn/cho-thue-can-ho-chung-cu-tp-hcm'
 driver.get(url)
 sleep(2)
 
@@ -55,7 +88,7 @@ URLs_all_page = []
 
 for page in range(input_page):
     URLs_one_page = geturl()
-    # print(geturl())
+    print(f"đã cào trang thứ {page+1}")
     sleep(2)
     a = page+2
     nexturl = url+"/p" + str(a)
@@ -63,9 +96,14 @@ for page in range(input_page):
     driver.get(nexturl)
     URLs_all_page = URLs_all_page + URLs_one_page
     sleep(2)
+    # if a < input_page:
+    #     nexturl = url+"/p" + str(a)
+    # # print(nexturl)
+    #     driver.get(nexturl)
+    #     URLs_all_page = URLs_all_page + URLs_one_page
+    #     sleep(2)
 print('- Finish Task 3: Scrape the URLs')
-print(URLs_all_page)
-
+#GHi số vào file
 b =2
 for linkbds in URLs_all_page:
     wa = load_workbook('ketquabds.xlsx')
@@ -76,62 +114,71 @@ for linkbds in URLs_all_page:
     was[tdlink1] = linkbds
     b = b + 1
     wa.save('ketquabds.xlsx')
+    wa.close()
     # print("có vào lưu file")
 
 print("lưu link rồi")
 
-# i = 2
-# for linkedin_URL in URLs_all_page:
-#     driver.get(linkedin_URL)
-#     print('- Accessing profile: ', linkedin_URL)
-#     page_source = BeautifulSoup(driver.page_source, "html.parser")
-#     info_div = page_source.find('div',{'class':'user'})
-#     info_loc = info_div.find_all('div')
-#     # print(info_loc)
-#     try:
-#         name = info_loc[1].get("title")
-#     except:
-#         name ="không có"
-#     #HÀM THAY ĐỔI    
-#     print(info_loc[2])
-#     ploai = info_loc[2].get("class")
-#     print(type(ploai))
-#     if ploai == ["info"]:
-#         sdtraw = info_loc[3].find("span")
-#         try:
-#             sdt= sdtraw.get("raw")
-#         except:
-#             sdt="không có"
-#         emailraw = info_loc[4].find("a")
-#         try:
-#             email =emailraw.get("data-email")
-#         except:
-#             email="không có"
-#     else:
-#         sdtraw = info_loc[2].find("span")
-#         try:
-#             sdt= sdtraw.get("raw")
-#         except:
-#             sdt="không có"
-#         emailraw = info_loc[3].find("a")
-#         try:
-#             email =emailraw.get("data-email")
-#         except:
-#             email="không có"
-#     # print(f"thông tin khách hàng tên {name} sdt là {sdt} có email là {email}")
-#     tdten = "a"+ str(i)
-#     tdsdt = "b"+ str(i)
-#     tdemail = "c" + str(i)
-#     # tdlink = "d" + str(i)
-#     wb = load_workbook('ketquabds.xlsx')
-#     activesheet = wb.worksheets[0]
-#     activesheet[tdten] = name
-#     activesheet[tdsdt] = sdt
-#     activesheet[tdemail] = email
-#     # activesheet[tdlink] = linkedin_URL
-#     i = i +1
-#     wb.save('ketquabds.xlsx')
-#     time.sleep(random.randint(1, 3))
-#     print(f"Đã xử lí xong {i} khách hàng")
+i= 2
+for linkurl in URLs_all_page:
+    #quanr li proxy
+    driver.get(linkurl)
+    # print('- Accessing profile: ', linkctcao)
+    page_source = BeautifulSoup(driver.page_source, "html.parser")
+    info_div = page_source.find('div',{'class':'user'})
+    # time.sleep(random.randint(2, 3))
+    try:
+        info_loc = info_div.find_all('div')
+        # print(info_loc)
+        try:
+            name = info_loc[1].get("title")
+        except:
+            name ="không có"
+        #HÀM THAY ĐỔI    
+        # print(info_loc[2])
+        ploai = info_loc[2].get("class")
+        # print(type(ploai))
+        if ploai == ["info"]:
+            sdtraw = info_loc[3].find("span")
+            try:
+                sdt= sdtraw.get("raw")
+            except:
+                sdt="không có"
+            emailraw = info_loc[4].find("a")
+            try:
+                email =emailraw.get("data-email")
+            except:
+                email="không có"
+        else:
+            sdtraw = info_loc[2].find("span")
+            try:
+                sdt= sdtraw.get("raw")
+            except:
+                sdt="không có"
+            emailraw = info_loc[3].find("a")
+            try:
+                email =emailraw.get("data-email")
+            except:
+                email="không có"
+        print(f"thông tin khách hàng tên {name} sdt là {sdt} có email là {email}")
+        tdten = "a"+ str(i)
+        tdsdt = "b"+ str(i)
+        tdemail = "c" + str(i)
+        # tdlink = "d" + str(i)
+        wa = load_workbook('ketquabds.xlsx')
+        activesheet = wa.worksheets[0]
+        activesheet[tdten] = name
+        activesheet[tdsdt] = sdt
+        activesheet[tdemail] = email
+        # activesheet[tdlink] = linkurl
+        i = i +1
+        wa.save('ketquabds.xlsx')
+        wa.close()
+        # time.sleep(random.randint(2, 3))
+    except:
+        pass
+        i = i +1
+    print(f"Đã xử lí xong {i-1} khách hàng")
+driver.quit()
+print('Mission Completed!')
 
-# print('Mission Completed!')
--- caonoidung.py
+++ caonoidung.py
@@ -11,13 +11,14 @@ import openpyxl
 import time
 import random
 
+
 wb = load_workbook('ketquabds.xlsx')
 activesheet = wb.worksheets[0]
 row_count = activesheet.max_row
 print(row_count)
 
 
-proxy = "27.79.140.226:48172"
+# proxy = "117.0.161.170:14514"
 # setup selenium webdriver
 opt = webdriver.ChromeOptions()
 # opt.add_extension("Block-image_v1.1.crx")
@@ -29,71 +30,84 @@ opt.add_argument("--start-maximized")
 opt.add_argument('--disable-dev-shm-usage')
 opt.add_argument('--no-sandbox')
 opt.add_argument('--ignore-certificate-errors')
-opt.add_argument("--disable-browser-side-navigation");
+opt.add_argument("--disable-browser-side-navigation")
 # opt.add_argument('--headless')
-opt.add_argument('--proxy-server={}'.format(proxy))
+# opt.add_argument('--proxy-server={}'.format(proxy))
 opt.add_argument("user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.90 Safari/537.36")
 # driver = webdriver.Chrome(options=opt)
-driver = webdriver.Chrome(options=opt)
+# driver = webdriver.Chrome(options=opt)
 
 
-i = 46
-linkcol= 2
+i = 70 # phai bat dau tu 2 vi 1 la tieu de
 for linkurl in range(row_count):
+    #quanr li proxy
+    if i % 40 == 0:
+        proxy = input('thay proxy mới đi ông:')
+    else:
+        pass
+    # opt.add_argument('--proxy-server={}'.format(proxy))
+    driver = webdriver.Chrome(options=opt)
+    #thuc hien cao link
     wb = load_workbook('ketquabds.xlsx')
     activesheet = wb.worksheets[0]
     linkvitri = "d" + str(i)
     linkctcao = activesheet[linkvitri].value
-    print(f"in linkctcao {linkctcao}")
+    # print(f"in linkctcao {linkctcao}")
     driver.get(linkctcao)
-    print('- Accessing profile: ', linkctcao)
+    # print('- Accessing profile: ', linkctcao)
     page_source = BeautifulSoup(driver.page_source, "html.parser")
     info_div = page_source.find('div',{'class':'user'})
-    info_loc = info_div.find_all('div')
-    # print(info_loc)
+    # time.sleep(random.randint(2, 3))
     try:
-        name = info_loc[1].get("title")
+        info_loc = info_div.find_all('div')
+        # print(info_loc)
+        try:
+            name = info_loc[1].get("title")
+        except:
+            name ="không có"
+        #HÀM THAY ĐỔI    
+        # print(info_loc[2])
+        ploai = info_loc[2].get("class")
+        # print(type(ploai))
+        if ploai == ["info"]:
+            sdtraw = info_loc[3].find("span")
+            try:
+                sdt= sdtraw.get("raw")
+            except:
+                sdt="không có"
+            emailraw = info_loc[4].find("a")
+            try:
+                email =emailraw.get("data-email")
+            except:
+                email="không có"
+        else:
+            sdtraw = info_loc[2].find("span")
+            try:
+                sdt= sdtraw.get("raw")
+            except:
+                sdt="không có"
+            emailraw = info_loc[3].find("a")
+            try:
+                email =emailraw.get("data-email")
+            except:
+                email="không có"
+        # print(f"thông tin khách hàng tên {name} sdt là {sdt} có email là {email}")
+        tdten = "a"+ str(i)
+        tdsdt = "b"+ str(i)
+        tdemail = "c" + str(i)
+        # tdlink = "d" + str(i)
+        activesheet = wb.worksheets[0]
+        activesheet[tdten] = name
+        activesheet[tdsdt] = sdt
+        activesheet[tdemail] = email
+        # activesheet[tdlink] = linkedin_URL
+        i = i +1
+        wb.save('ketquabds.xlsx')
+        # time.sleep(random.randint(2, 3))
     except:
-        name ="không có"
-    #HÀM THAY ĐỔI    
-    print(info_loc[2])
-    ploai = info_loc[2].get("class")
-    print(type(ploai))
-    if ploai == ["info"]:
-        sdtraw = info_loc[3].find("span")
-        try:
-            sdt= sdtraw.get("raw")
-        except:
-            sdt="không có"
-        emailraw = info_loc[4].find("a")
-        try:
-            email =emailraw.get("data-email")
-        except:
-            email="không có"
-    else:
-        sdtraw = info_loc[2].find("span")
-        try:
-            sdt= sdtraw.get("raw")
-        except:
-            sdt="không có"
-        emailraw = info_loc[3].find("a")
-        try:
-            email =emailraw.get("data-email")
-        except:
-            email="không có"
-    # print(f"thông tin khách hàng tên {name} sdt là {sdt} có email là {email}")
-    tdten = "a"+ str(i)
-    tdsdt = "b"+ str(i)
-    tdemail = "c" + str(i)
-    # tdlink = "d" + str(i)
-    activesheet = wb.worksheets[0]
-    activesheet[tdten] = name
-    activesheet[tdsdt] = sdt
-    activesheet[tdemail] = email
-    # activesheet[tdlink] = linkedin_URL
+        pass
     i = i +1
-    wb.save('ketquabds.xlsx')
-    time.sleep(random.randint(2, 5))
-    print(f"Đã xử lí xong {i} khách hàng")
+    driver.quit()
+    print(f"Đã xử lí xong {i-1} khách hàng")
 
 print('Mission Completed!')
